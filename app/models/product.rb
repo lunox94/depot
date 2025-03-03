b35ab2ed3ec11ade8b3aa7408873fb95f35e0302
lunox94 <gmrlunox94@gmail.com>
2025-03-03 11:48:41 +0000

    Add validation rules for Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+  validate :acceptable_image
+  validates :title, uniqueness: true
+  validates :description, :image, :title, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  def acceptable_image
+    return unless image.attached?
+
+    puts image.content_type
+
+    acceptable_types = [ "image/jpeg", "image/gif", "image/png" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a JPG, GIF or PNG image")
+    end
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "products attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:price].any?
+    assert product.errors[:title].any?
+    assert product.errors[:image].any?
+    assert product.errors[:description].any?
+  end
+
+  test "product price must be possitive" do
+    product = Product.new(description: "xyz", title: "xyz")
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    product = Product.new(description: "xyz", price: 1, title: "xyz")
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+
+    assert product.valid?, "image/jpeg must be valid"
+
+    product = Product.new(description: "xyz", price: 1, title: "xyz")
+    product.image.attach(io: File.open("test/fixtures/files/logo.svg"), filename: "logo.svg", content_type: "image/svg+xml")
+
+    assert_not product.valid?, "image/svg+xml must be invalid"
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(description: "xyz", price: 1, title: products(:pragprog).title)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+
+    assert product.invalid?
+
+    assert [ I18n.translate("errors.messages.taken") ], product.errors[:title]
+  end
 end
