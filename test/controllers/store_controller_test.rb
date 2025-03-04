ed5b3a5c46724b9e7298310a76cbf2c31e3ec3dd
lunox94 <gmrlunox94@gmail.com>
2025-03-04 12:48:09 +0000

    Add date and time to the nav bar and update currency unit

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -7,6 +7,13 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select "nav a", minimum: 4
     assert_select "main ul li", 3
     assert_select "h2", "The Pragmatic Programmer"
-    assert_select "div", /\$[,\d]+\.\d\d/
+    assert_select "div", /€[,\d]+\.\d\d/
+  end
+
+  test "should display date and time" do
+    travel_to Time.new(2025, 3, 4, 14, 30, 0) do
+      get store_index_url
+      assert_select "nav li", I18n.l(Time.current, format: :short)
+    end
   end
 end
