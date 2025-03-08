988ba593a18eabd8e7f31c67c089831bf4e5e5f6
lunox94 <gmrlunox94@gmail.com>
2025-03-08 17:23:16 +0000

    Add tests for Cart#add_product

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = Cart.new
+  end
+
+  test "add a single product to the cart" do
+    one = products(:one)
+    pragprog = products(:pragprog)
+
+    @cart.add_product(one).save!
+    @cart.add_product(pragprog).save!
+
+    assert_equal @cart.line_items.count, 2
+    assert_equal @cart.total_price, one.price + pragprog.price
+  end
+
+  test "add multiple products to the cart" do
+    one = products(:one)
+    pragprog = products(:pragprog)
+
+    @cart.add_product(one).save!
+
+    3.times do
+      @cart.add_product(pragprog).save!
+    end
+
+    assert_equal @cart.line_items.count, 2
+    assert_equal @cart.total_price, one.price + pragprog.price * 3
+  end
 end
