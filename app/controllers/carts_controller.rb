681674dfc740d3beb2e58b596e7f0c07cd2aa680
lunox94 <gmrlunox94@gmail.com>
2025-03-12 17:06:19 +0000

    Prevent page refresh when emptying the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,6 +55,13 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          :cart,
+          partial: "layouts/cart",
+          object: nil
+        )
+      end
       format.html { redirect_to store_index_path, status: :see_other, notice: "Your cart is currently empty" }
       format.json { head :no_content }
     end
