2dae84fd9e3bd10e2b53c601d67072b83815b83c
lunox94 <gmrlunox94@gmail.com>
2025-03-14 15:13:57 +0000

    Create new orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,19 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
   enum :pay_type, {
     "Check"           => 0,
     "Credit card"     => 1,
     "Purchase order"  => 2
   }
+
+  validates :address, :email, :name, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |li|
+      li.cart_id = nil
+      line_items << li
+    end
+  end
 end
