aa7b872150bd5bfefb5ff7de6e5cd32789de701c
lunox94 <gmrlunox94@gmail.com>
2025-03-16 17:54:07 +0000

    Run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_13_211326) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_16_174329) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -64,6 +64,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_13_211326) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "credit_card_number"
+    t.date "expiration_date"
+    t.string "routing_number"
+    t.string "account_number"
+    t.integer "po_number"
   end
 
   create_table "products", force: :cascade do |t|
