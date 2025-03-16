9a3e98021397b287379b95b685d49665918bd99c
lunox94 <gmrlunox94@gmail.com>
2025-03-16 09:58:12 +0000

    Configure system tests to run in CI and locally

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
+unless ENV["CI"]
+  Capybara.server_host = "0.0.0.0"
+  Capybara.app_host = "http://app:3000" # Use service name from docker-compose.yml
+  Capybara.server_port = 3000
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  if ENV["CI"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  else
+    driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+      browser: :remote,
+      url: "http://chrome:4444/wd/hub"
+    }
+  end
 end
