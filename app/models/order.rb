fc9a707f3bb7ed58d89b11f0ea1aa1950abc1db1
lunox94 <gmrlunox94@gmail.com>
2025-03-19 18:43:22 +0000

    Integrate Pago mock payment processor

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+require "pago"
+
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
@@ -18,6 +20,38 @@ class Order < ApplicationRecord
     cart.line_items.each do |li|
       li.cart_id = nil
       line_items << li
+    end
+  end
+
+  def charge!
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when "Check"
+      payment_method = :check
+      payment_details[:routing] = routing_number
+      payment_details[:account] = account_number
+    when "Credit card"
+      payment_method = :credit_card
+      payment_details[:cc_num] = credit_card_number
+      payment_details[:expiration_month] = expiration_date.month
+      payment_details[:expiration_year] = expiration_date.year
+    when "Purchase order"
+      payment_method = :po
+      payment_details[:po_num] = po_number
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method: payment_method,
+      payment_details: payment_details
+    )
+
+    if payment_result.success?
+      OrderMailer.received(self)
+    else
+      raise payment_result.error
     end
   end
 end
