e9599fc2de8c246a7d1de8bcfc2d9f5ca1c7ace4
lunox94 <gmrlunox94@gmail.com>
2025-03-21 21:21:00 +0000

    Add field validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :email_address, presence: true, uniqueness: true
   has_secure_password
   has_many :sessions, dependent: :destroy
 
