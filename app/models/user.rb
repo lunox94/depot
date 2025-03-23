bcaf3467d90e0bca5db686c865863534dcaf8c72
lunox94 <gmrlunox94@gmail.com>
2025-03-23 17:37:04 +0000

    Not always require the password challenge

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   validates :email_address, presence: true, uniqueness: true
 
   has_secure_password
-  validates :password_challenge, presence: true, on: :update
+  validates :password_challenge, presence: true, on: :update, if: :password_digest_changed?
 
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   after_destroy :ensure_an_admin_remains
+
+  before_validation :clear_password_challenge
 
   class Error < StandardError
   end
@@ -18,6 +20,12 @@ class User < ApplicationRecord
     def ensure_an_admin_remains
       if User.count.zero?
         raise Error.new "Can't delete last user"
+      end
+    end
+
+    def clear_password_challenge
+      unless password_digest_changed?
+        @password_challenge = nil
       end
     end
 end
