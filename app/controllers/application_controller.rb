566a54ed110141398b3de1df68abf33f090b0ba9
lunox94 <gmrlunox94@gmail.com>
2025-03-23 18:15:37 +0000

    Configure i18n for non administration pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include ActiveStorage::SetCurrent
+
+  before_action :set_i18n_locale_from_params
+
+  private
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   resources :users
   resource :session
   resources :passwords, param: :token
-  resources :orders
-  resources :line_items
-  resources :carts
-  root "store#index", as: "store_index"
   resources :products
+
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: "store_index", via: :all
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
