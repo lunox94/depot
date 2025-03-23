3f41cb20474aafe84bae1c1cd2ec9d21829b1da0
lunox94 <gmrlunox94@gmail.com>
2025-03-23 10:39:37 +0000

    Create default user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,26 @@ class SessionsController < ApplicationController
   def new
   end
 
+
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
-      redirect_to after_authentication_url
-    else
-      redirect_to new_session_path, alert: "Try another email address or password."
-    end
+    ensure_default_user if User.count.zero?
+
+    user_params = params.permit(:email_address, :password)
+    return redirect_to new_session_path, alert: "Try another email address or password." unless (user = User.authenticate_by(user_params))
+
+    start_new_session_for(user)
+    redirect_to after_authentication_url
   end
 
   def destroy
     terminate_session
     redirect_to new_session_path
+  end
+
+  private
+
+  def ensure_default_user
+    user_params = params.permit(:email_address, :password).merge(name: "Default")
+    User.create!(user_params)
   end
 end
