f289baaa19f7c2c728fe439f63a68ff312691821
lunox94 <gmrlunox94@gmail.com>
2025-04-02 13:03:09 +0000

    --amend

--- db/migrate/20250402125953_create_support_requests.rb
+++ db/migrate/20250402125953_create_support_requests.rb
@@ -1,6 +1,10 @@
 class CreateSupportRequests < ActiveRecord::Migration[8.0]
   def change
     create_table :support_requests do |t|
+      t.string :email, comment: "Email of the submitter"
+      t.string :subject, comment: "Subject of the support request"
+      t.text :message, comment: "Message content of the support request"
+      t.references :order, foreign_key: true, comment: "Reference to the order associated with the support request"
       t.timestamps
     end
   end
