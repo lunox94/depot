62660f16385cbd0f33f63b26515ca9e9a10a48d8
lunox94 <gmrlunox94@gmail.com>
2025-04-02 15:00:22 +0000

    Add tests for the SupportMailbox

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,4 +1,12 @@
 class SupportMailbox < ApplicationMailbox
   def process
+    recent_order = Order.where(email: mail.from_address.to_s).order("created_at desc").first
+
+    SupportRequest.create!(
+      email: mail.from_address.to_s,
+      message: mail.body.to_s,
+      order: recent_order,
+      subject: mail.subject
+    )
   end
 end
--- test/mailboxes/support_mailbox_test.rb
+++ test/mailboxes/support_mailbox_test.rb
@@ -1,11 +1,35 @@
 require "test_helper"
 
 class SupportMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test "we create a SupportRequest when we receive mail" do
+    receive_inbound_email_from_mail \
+      to: "support@example.com",
+      from: "customer@example.com",
+      subject: "I need help",
+      body: "I can't find my order"
+
+    support_request = SupportRequest.last
+
+    assert_equal "customer@example.com", support_request.email
+    assert_equal "I need help", support_request.subject
+    assert_equal "I can't find my order", support_request.message
+    assert_nil support_request.order
+  end
+
+  test "we create a SupportRequest with the most recent order" do
+    recent_order = orders(:david_second_order)
+
+    receive_inbound_email_from_mail \
+      to: "support@example.com",
+      from: recent_order.email,
+      subject: "I need help",
+      body: "I can't find my order"
+
+    support_request = SupportRequest.last
+
+    assert_equal recent_order.email, support_request.email
+    assert_equal "I need help", support_request.subject
+    assert_equal "I can't find my order", support_request.message
+    assert_equal recent_order, support_request.order
+  end
 end
